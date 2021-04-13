467f4b6ea752edff2022ff72bb1c7047689ad466
marvinblanca <markmarvinblanca@gmail.com>
2021-04-13 13:07:05 +0800

    - Setup a simple implementation of Navigation Component

--- app/src/main/java/com/kwartracker/android/MainActivity.kt
+++ app/src/main/java/com/kwartracker/android/MainActivity.kt
@@ -2,10 +2,22 @@ package com.kwartracker.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+    private val navController: NavController
+        get() = findNavController(R.id.nav_host_fragment)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
