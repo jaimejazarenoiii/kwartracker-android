9e0d5b517fd234c20e2339814785d8e5455c7113
Danace Bryant Alfonso <danacebryantalfonso@LIGs-MacBook-Air.local>
2021-04-15 14:56:37 +0800

    Initial Commit

---
+++ app/src/main/java/com/kwartracker/android/signup/ui/SignUpFragment.kt
@@ -0,0 +1,34 @@
+package com.kwartracker.android.signup.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.kwartracker.android.R
+
+class SignUpFragment : Fragment() {
+
+    private lateinit var signUpViewModel: SignUpViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        signUpViewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_signup, container, false)
+        /*val textView: TextView = root.findViewById(R.id.text_slideshow)
+        signUpViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })*/
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
