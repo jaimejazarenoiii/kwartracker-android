7bbede2c4eaa1552eaf8e6e1ebe7ebe7302768ad
Danace Bryant Alfonso <danacebryantalfonso@LIGs-MacBook-Air.local>
2021-04-19 12:09:54 +0800

    Added navigation back

--- app/src/main/java/com/kwartracker/android/signup/ui/SignUpFragment.kt
+++ app/src/main/java/com/kwartracker/android/signup/ui/SignUpFragment.kt
@@ -2,22 +2,33 @@ package com.kwartracker.android.signup.ui
 
 import android.os.Bundle
 import android.view.*
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.kwartracker.android.R
+
 
 class SignUpFragment : Fragment() {
 
     private lateinit var signUpViewModel: SignUpViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         signUpViewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_signup, container, false)
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val tvSignIn: TextView = view.findViewById(R.id.tv_signin)
+        tvSignIn.setOnClickListener {
+            getFragmentManager()?.popBackStack()
+        }
     }
 }
