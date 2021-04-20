e9dab1ec540bcebb3c1e14c549e59639e785a85a
jupilot11 <lariosajupilot1@gmail.com>
2021-04-20 09:48:28 +0800

    - Update min SDK Version - Fix conflict design (WIP) - Implement view binding for xml for both login and sign up - Implement simple navigation between fragments

---
+++ app/src/main/java/com/kwartracker/android/KwartrackerApplication.kt
@@ -0,0 +1,10 @@
+package com.kwartracker.android
+
+import android.app.Application
+
+class KwartrackerApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/kwartracker/android/signup/ui/SignUpFragment.kt
+++ app/src/main/java/com/kwartracker/android/signup/ui/SignUpFragment.kt
@@ -3,31 +3,33 @@ package com.kwartracker.android.signup.ui
 import android.os.Bundle
 import android.view.*
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.kwartracker.android.R
+import com.kwartracker.android.databinding.FragmentLoginBinding
+import com.kwartracker.android.databinding.FragmentSignupBinding
 
 
 class SignUpFragment : Fragment() {
+
     private lateinit var signUpViewModel: SignUpViewModel
+    lateinit var binding: FragmentSignupBinding
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
-        signUpViewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_signup, container, false)
-
-        return root
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_signup, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val tvSignIn: TextView = view.findViewById(R.id.tv_signin)
-        tvSignIn.setOnClickListener {
-            getFragmentManager()?.popBackStack()
+        binding.tvSignin.setOnClickListener {
+            findNavController().popBackStack(R.id.loginFragment, false)
         }
     }
 }
