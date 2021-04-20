f20e8c3f9d9b439d06f632d70f6288b3621d597f
jupilot11 <lariosajupilot1@gmail.com>
2021-04-20 10:56:11 +0800

    -remove extra header in activity_main.xml

--- app/src/main/java/com/kwartracker/android/signup/ui/SignUpFragment.kt
+++ app/src/main/java/com/kwartracker/android/signup/ui/SignUpFragment.kt
@@ -29,7 +29,7 @@ class SignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.tvSignin.setOnClickListener {
-            findNavController().popBackStack(R.id.loginFragment, false)
+            findNavController().navigate(R.id.action_signupFragment_to_loginFragment)
         }
     }
 }
