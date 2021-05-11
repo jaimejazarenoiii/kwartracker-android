fbc0b54c9aa3e298987706a3c9aebd52200b7387
Danace Bryant Alfonso <danacebryantalfonso@LIGs-MacBook-Air.local>
2021-05-11 13:42:03 +0800

    Pulled the latest develop branch

--- app/src/main/java/com/kwartracker/android/KwartrackerApplication.kt
+++ app/src/main/java/com/kwartracker/android/KwartrackerApplication.kt
@@ -2,4 +2,9 @@ package com.kwartracker.android
 
 import android.app.Application
 
-class KwartrackerApplication : Application()
+class KwartrackerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
