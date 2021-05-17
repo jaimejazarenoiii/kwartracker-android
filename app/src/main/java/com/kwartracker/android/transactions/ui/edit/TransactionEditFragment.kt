3abe06297db8983fa2c54fc85718ae8540bc4c94
Danace Bryant Alfonso <danacebryantalfonso@LIGs-MacBook-Air.local>
2021-05-17 18:10:10 +0800

    Create transaction edit UI

--- app/src/main/java/com/kwartracker/android/transactions/ui/edit/TransactionEditFragment.kt
+++ app/src/main/java/com/kwartracker/android/transactions/ui/edit/TransactionEditFragment.kt
@@ -1,4 +1,4 @@
-package com.kwartracker.android.transactions.ui.add
+package com.kwartracker.android.transactions.ui.edit
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,14 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.kwartracker.android.R
-import com.kwartracker.android.databinding.FragmentTransactionAddBinding
+import com.kwartracker.android.databinding.FragmentTransactionEditBinding
 import com.kwartracker.android.transactions.ui.list.TransactionsListFragment
 import com.kwartracker.android.transactions.ui.main.TransactionsFragment
 
-class TransactionAddFragment : Fragment() {
-    lateinit var binding: FragmentTransactionAddBinding
+class TransactionEditFragment : Fragment() {
+    lateinit var binding: FragmentTransactionEditBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,7 +21,7 @@ class TransactionAddFragment : Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(
             inflater,
-            R.layout.fragment_transaction_add,
+            R.layout.fragment_transaction_edit,
             container, false
         )
 
@@ -30,10 +29,6 @@ class TransactionAddFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.btnNext.setOnClickListener {
-            findNavController().navigate(R.id.transaction_add_1_fragment)
-        }
-
         binding.btnBack.setOnClickListener {
             TransactionsFragment().bottomMainSheetModal(TransactionsListFragment())
         }
