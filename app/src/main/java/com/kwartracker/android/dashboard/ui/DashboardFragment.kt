3250edaa2043bcd095a66a80d221fc10d2c1802a
jupilot11 <lariosajupilot1@gmail.com>
2021-05-17 12:05:49 +0800

    - implement dashboard navigation to respective fragment from Side navigation control

--- app/src/main/java/com/kwartracker/android/dashboard/ui/DashboardFragment.kt
+++ app/src/main/java/com/kwartracker/android/dashboard/ui/DashboardFragment.kt
@@ -5,14 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.app.ActivityCompat.invalidateOptionsMenu
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.kwartracker.android.R
 import com.kwartracker.android.databinding.FragmentDashboardBinding
-import com.kwartracker.android.databinding.FragmentWalletsBinding
 
 class DashboardFragment : Fragment() {
 
@@ -34,6 +34,24 @@ class DashboardFragment : Fragment() {
         actionBarDrawerToggle?.let { binding.drawerLayout.addDrawerListener(it) }
         binding.drawerLayout.setScrimColor(Color.TRANSPARENT)
         binding.drawerLayout.drawerElevation = 0f
+        binding.navView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    Toast.makeText(requireContext(), "Home", Toast.LENGTH_SHORT).show()
+                }
+                R.id.walletFragment -> {
+                    findNavController().navigate(R.id.action_dashoardFragment_to_walletsFragment)
+                }
+                R.id.transactionFragment -> {
+                    findNavController().navigate(R.id.action_dashoardFragment_to_transactions_fragment)
+                }
+                R.id.profileFragment -> {
+                    findNavController().navigate(R.id.action_dashoardFragment_to_profile_fragment)
+                }
+            }
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            return@setNavigationItemSelectedListener true
+        }
         binding.navView.inflateHeaderView(R.layout.navigation_header)
         binding.btnDrawer.setOnClickListener {
             binding.drawerLayout.openDrawer(GravityCompat.START)
@@ -41,7 +59,13 @@ class DashboardFragment : Fragment() {
     }
 
     private fun setupDrawerToggle(): ActionBarDrawerToggle? {
-        return object : ActionBarDrawerToggle(requireActivity(), binding.drawerLayout, null, R.string.drawer_open, R.string.drawer_close) {
+        return object : ActionBarDrawerToggle(
+            requireActivity(),
+            binding.drawerLayout,
+            null,
+            R.string.drawer_open,
+            R.string.drawer_close
+        ) {
             override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
                 super.onDrawerSlide(drawerView, slideOffset)
                 binding.linearlayoutMainMenu.translationX = slideOffset * drawerView.width
