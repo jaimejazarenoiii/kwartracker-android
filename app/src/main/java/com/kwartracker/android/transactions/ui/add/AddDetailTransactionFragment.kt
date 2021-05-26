a2e1c2ffa710a821e7805544c07e186b4ad30e07
danacealfonso <danace_bboy@gmail.com>
2021-05-26 11:12:10 +0800

    Add save button in add detail transaction

--- app/src/main/java/com/kwartracker/android/transactions/ui/add/AddDetailTransactionFragment.kt
+++ app/src/main/java/com/kwartracker/android/transactions/ui/add/AddDetailTransactionFragment.kt
@@ -1,5 +1,6 @@
 package com.kwartracker.android.transactions.ui.add
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.kwartracker.android.R
 import com.kwartracker.android.databinding.FragmentAddDetailTransactionBinding
+import com.kwartracker.android.widgets.ConfirmedDialog
 
 class AddDetailTransactionFragment : Fragment() {
     lateinit var binding: FragmentAddDetailTransactionBinding
@@ -28,8 +30,17 @@ class AddDetailTransactionFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val confirmedDialog = ConfirmedDialog(activity as Activity)
         binding.btnBack.setOnClickListener {
-            findNavController().navigate(R.id.add_wallet_transaction_fragment)
+            findNavController().navigate(R.id.transaction_fragment)
+        }
+        binding.btnAdd.setOnClickListener {
+            confirmedDialog.setIcon(R.drawable.ic_success_teal_75)
+            confirmedDialog.setTitle(getString(R.string.lbl_success))
+            confirmedDialog.setExitText(getString(R.string.lbl_cool))
+            confirmedDialog.setMessage(getString(R.string.lbl_confirmed_success_message))
+            confirmedDialog.show()
+            findNavController().navigate(R.id.transaction_fragment)
         }
     }
 }
