743081832084db9ae18934f504b78ea1235438df
marvinblanca <markmarvinblanca@gmail.com>
2021-07-06 18:02:26 +0800

    - Create reports dashboard UI (Android Studio Beta)

--- app/src/main/java/com/kwartracker/android/dashboard/ui/DashboardFragment.kt
+++ app/src/main/java/com/kwartracker/android/dashboard/ui/DashboardFragment.kt
@@ -48,7 +48,9 @@ class DashboardFragment : Fragment() {
                     )
                 }
                 R.id.reportFragment -> {
-                    findNavController().navigate(R.id.action_dashoardFragment_to_reportsDashboardFragment)
+                    findNavController().navigate(
+                        R.id.action_dashoardFragment_to_reportsDashboardFragment
+                    )
                 }
                 R.id.profileFragment -> {
                     findNavController().navigate(R.id.action_dashoardFragment_to_profile_fragment)
